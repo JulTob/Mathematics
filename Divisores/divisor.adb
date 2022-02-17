d7b1168447e11e2e144f7528bff65108e845dd8d
Julio <JulioToboso@gmail.com>
2022-02-17 02:14:32 +0100

    Create divisor.adb

---
+++ Divisores/divisor.adb
@@ -0,0 +1,15 @@
+with Ada.Text_IO; use Ada.Text_IO;
+With Ada.Integer_Text_IO; use Ada.Integer_Text_IO; 
+
+
+procedure Hello is
+    N : Natural := 156;
+    d : Natural := 1;
+begin
+  while d <= n loop  
+    if n mod d = 0 then
+      put(d);
+    end if;
+    d := d+1;
+  end loop;
+end Hello;
