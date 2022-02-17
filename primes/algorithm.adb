f528d8a3ffa3f077bbeef3d804f696380cdf8a87
Julio <JulioToboso@gmail.com>
2022-02-17 23:31:15 +0100

    Create algorithm.adb

---
+++ primes/algorithm.adb
@@ -0,0 +1,58 @@
+with Ada.Text_IO; use Ada.Text_IO;
+With Ada.Integer_Text_IO; use Ada.Integer_Text_IO; 
+
+
+procedure algorithm is
+    function is_Prime(
+        N: in Natural) return Boolean
+        is
+        begin
+        --if N = 1  then return false; end if;  -- (:
+            for i in 2..N/2 loop
+                if  is_Prime(i) and then 
+                    N rem i = 0 then
+                        return false;
+                end if;
+            end loop;
+            return true;
+        end Is_Prime;
+        
+        
+    procedure factorization
+        (
+        N: in Natural)
+        is
+        M: Natural := N;
+        i: Natural := 2;
+        begin
+        while i <= M loop
+            if i = M then
+                Put(i,1);
+                M := M/i;
+                end if;
+
+            if M rem i = 0 then
+                Put(i,1); Put(" · ");
+                M := M/i;
+            else
+                i := i+1;
+                end if;
+        end loop;
+        
+        end factorization;
+    
+   -- X : Natural := 15;
+begin
+for x in 1..1000 loop
+    Put("Factors: ");
+    put(X,1); Put(" = "); 
+    factorization(x);
+    Put(" "); 
+    
+    if is_prime(x) then
+        put("Is prime");
+        end if;
+        
+    New_Line;
+    end loop;
+end algorithm;
