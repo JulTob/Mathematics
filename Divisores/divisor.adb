8eff1704ac193f07ef32d2f73dd1da4261516a5f
Julio <JulioToboso@gmail.com>
2022-04-28 22:29:24 +0200

    Update divisor.adb

--- Divisores/divisor.adb
+++ Divisores/divisor.adb
@@ -3,13 +3,14 @@ With Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 
 procedure Divisor is
-    N : Natural := 156;
+    N : Natural := 365*24*60*60 + 6*60*60; -- Seconds in a year
     d : Natural := 1;
 begin
+  put(n);   New_Line; 
   while d*d <= n loop  
     if n mod d = 0 then
       put(d);
-    end if;
+      end if;
     d := d+1;
-  end loop;
-end Divisor;
+    end loop;
+  end Divisor;
