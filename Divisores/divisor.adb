fd1f70d2806025423993dc8105ac695d41157128
Julio <JulioToboso@gmail.com>
2022-04-28 22:02:39 +0200

    Update divisor.adb

--- Divisores/divisor.adb
+++ Divisores/divisor.adb
@@ -2,14 +2,14 @@ with Ada.Text_IO; use Ada.Text_IO;
 With Ada.Integer_Text_IO; use Ada.Integer_Text_IO; 
 
 
-procedure Hello is
+procedure Divisor is
     N : Natural := 156;
     d : Natural := 1;
 begin
-  while d <= n loop  
+  while d*d <= n loop  
     if n mod d = 0 then
       put(d);
     end if;
     d := d+1;
   end loop;
-end Hello;
+end Divisor;
