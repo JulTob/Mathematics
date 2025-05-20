220acff9d304aa2cd96ab3c6852aac92acc9d9bc
Julio Toboso <JulioToboso@gmail.com>
2025-05-20 16:42:58 +0200

    Update Vectors.js

--- docs/picasso/Vectors.js
+++ docs/picasso/Vectors.js
@@ -3,7 +3,17 @@ export class Vector {
         this.dx = dx;
         this.dy = dy;
         }
-  
+
+    add(other) {
+        if (!(other instanceof Vector)) throw new Error("Expected Vector");
+        return new Vector(this.dx + other.dx, this.dy + other.dy);
+        }
+    
+    scale(scalar) {
+        return new Vector(this.dx * scalar, this.dy * scalar);
+        }
+
+
     applyTo(point) {
         if (!(point instanceof Point)) {
             throw new Error("Expected a Point as argument");
@@ -11,3 +21,5 @@ export class Vector {
         return new Point(point.x + this.dx, point.y + this.dy);
         }
     }
+
+window.Vector = Vector;
